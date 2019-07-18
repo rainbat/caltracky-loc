42b3d10e8d5e96f8153859c7e5f730606baaa8bc
Dominique Dümmler <info@rainbat.ch>
2019-07-18 20:58:00 +0200

    update

--- copy.sh
+++ copy.sh
@@ -9,15 +9,18 @@ git add .
 git commit -m "update" .
 git push origin
 
-
 #pgb login
 pgb unlock ios 1082720
 pgb unlock android 390090
 pgb build 2834269
 
-pgb download 2834269 ios
-pgb download 2834269 android
+rm *.apk
+rm *.ipa
 
+pgb download 2834269 ios CalTracky.ipa
+pgb download 2834269 android CalTracky.apk
+
+/Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Support/altool --upload-app -f CalTracky.ipa -u info@rainbat.ch -peqyd-grgm-evwp-ywaa
 #xcodebuild  -UseModernBuildSystem=NO -workspace platforms/ios/CalTracky.xcworkspace/ -scheme "CalTracky" -destination generic/platform=iOS build archive
 #ios-deploy --bundle /Users/rainbat/Library/Developer/Xcode/DerivedData/CalTracky-gkfgmoaprtzowqghppsgomcklesk/Build/Products/Debug-iphoneos/CalTracky.app --no-wifi
 #https://medium.com/xcblog/xcodebuild-deploy-ios-app-from-command-line-c6defff0d8b8
