e4db5b08ae29b4f3e57cea5fc07f677bd531a722
Dominique Dümmler <info@rainbat.ch>
2019-08-20 17:08:11 +0200

    update

--- copy.sh
+++ copy.sh
@@ -19,13 +19,13 @@ git commit -m "update" .
 git push origin
 
 #pgb login
-pgb unlock ios 1082720
-pgb unlock android 390090
-pgb update 2834269
+#pgb unlock ios 1082720
+#pgb unlock android 390090
+#pgb update 2834269
 #pgb build 2834269
 
-pgb download 2834269 ios CalTracky.ipa
-pgb download 2834269 android CalTracky.apk
+#pgb download 2834269 ios CalTracky.ipa
+#pgb download 2834269 android CalTracky.apk
 
 #/Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Support/altool --upload-app -f CalTracky.ipa -u info@rainbat.ch -peqyd-grgm-evwp-ywaa
 
